dd56355988768fa139fc2e4c8d5da62d82a87ceb
Jonathan Langlois <jon@button.is>
2021-05-13 12:04:06 -0700

    clean up warnings
    fix an empty object warning, unused variable

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import L from 'leaflet';
 import "leaflet/dist/leaflet.css";
-import {swapCoords, getRiskIcon} from '../helpers';
+import { getRiskIcon } from '../helpers';
 import {getAllMinesData} from '../services/ai-service';
 
 // Leaflet Icons setup
@@ -16,7 +16,7 @@ L.Marker.prototype.options.icon = DefaultIcon;
 
 const victoriaCoords = [48.407326, -123.329773]
 
-export default function LeafletMap({}) {
+export default function LeafletMap() {
   const [markers, setMarkers] = useState([])
   useEffect(() => {
     getAllMinesData()
